51826637af41393a4976a55bab8597cf57ca903d
Gabriel Andino <gabriel_andino@outlook.com>
2020-09-09 13:34:43 -0300

    Agregando las url de registro, login y logout

--- ProyectoCarnet/ProyectoCarnet/urls.py
+++ ProyectoCarnet/ProyectoCarnet/urls.py
@@ -17,5 +17,8 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
     path('admin/', admin.site.urls),
 ]
