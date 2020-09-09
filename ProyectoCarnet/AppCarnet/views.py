e9e0603df27a23fded47df9f2e4ba91d25f89cc9
Gabriel Andino <gabriel_andino@outlook.com>
2020-09-09 13:37:20 -0300

    Agregando las librerias para views de registrar, loguearse y logout

--- ProyectoCarnet/AppCarnet/views.py
+++ ProyectoCarnet/AppCarnet/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, Http404
+from .forms import forms, CreateUserForm
 
 # Create your views here.
