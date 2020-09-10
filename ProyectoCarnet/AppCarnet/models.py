afa9e8ca712688cd123d8c3e4001a36e4fe089d5
Lucas Carabajal <lucascar007@gmail.com>
2020-09-10 17:02:37 -0300

    Cambios en los models agregando la clase User 	# Please enter the commit message for your changes. Lines starting

--- ProyectoCarnet/AppCarnet/models.py
+++ ProyectoCarnet/AppCarnet/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
-"""class GrupoSanguineo(models.Model):
+class GrupoSanguineo(models.Model):
     grupo = models.CharField(max_length=3)
 
 
@@ -13,7 +14,7 @@ class Nacionalidad(models.Model):
 class Persona(models.Model):
     nombre = models.CharField(max_length=50) 
     apellido = models.CharField(max_length=50)
-    dni = models.IntegerField(max_length=10)
+    dni = models.IntegerField(primary_key=True)
     nacionalidad = models.ForeignKey(Nacionalidad, on_delete=models.PROTECT)
     direccion = models.CharField(max_length=70)
     grupo_s = models.ForeignKey(GrupoSanguineo, on_delete=models.PROTECT)
@@ -33,11 +34,10 @@ class Carnet(models.Model):
     tipo_carnet = models.ForeignKey(TipoCarnet, on_delete=models.PROTECT)
 
 
-class Usuario(models.Model):
-    nickname = models.CharField(max_length=25)
-    password = models.CharField(max_length=25)
-    email = models.EmailField()
-    n_carnet = models.ForeignKey(Carnet, on_delete=models.CASCADE)
+class Usuario(User):
+    user = models.OneToOneField(User, parent_link= True, on_delete=models.CASCADE)
+    persona = models.OneToOneField(Persona, on_delete = models.CASCADE)
+    n_carnet = models.OneToOneField(Carnet, on_delete=models.CASCADE)
 
 
 class Marca(models.Model):
@@ -51,6 +51,16 @@ class Modelo(models.Model):
 class TipoVehiculo(models.Model):
     nombre = models.CharField(max_length=50)
 
+class TipoSeguro(models.Model):
+    nombre = models.CharField(max_length=30)
+
+class Seguro(models.Model):
+    nombre = models.CharField(max_length=50)
+    num_poliza = models.IntegerField(max_length=30)
+    tel = models.IntegerField(max_length=15)
+    tel_emergencia = models.IntegerField(max_length=15)
+    tipo = models.ForeignKey(TipoSeguro, on_delete=models.PROTECT)    
+
 class Cedula(models.Model):
     num_cedula = models.CharField(max_length= 45)
     patente = models.CharField(max_length= 15)
@@ -61,16 +71,10 @@ class Cedula(models.Model):
     emision = models.DateField()
     vencimiento = models.DateField()
     seguro = models.ForeignKey(Seguro, on_delete=models.PROTECT)
-    tipo = models.ForeignKey(TipoVehiculo)
+    tipo = models.ForeignKey(TipoVehiculo, on_delete=models.PROTECT)
 
 
-class TipoSeguro(models.Model):
-    nombre = models.CharField(max_length=30)
 
-class Seguro(models.Model):
-    nombre = models.CharField(max_length=50)
-    num_poliza = models.IntegerField(max_length=30)
-    tel = models.IntegerField(max_length=15)
-    tel_emergencia = models.IntegerField(max_length=15)
-    tipo = models.ForeignKey(TipoSeguro, on_delete=models.PROTECT)
-"""
+
+
+
