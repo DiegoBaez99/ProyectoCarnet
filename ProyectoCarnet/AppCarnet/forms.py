df186036e0a408cad640497e1cd781ff6d92057f
Gabriel Andino <gabriel_andino@outlook.com>
2020-09-15 18:02:55 -0300

    Cambio el signup, ahora tiene un form cheto

--- ProyectoCarnet/AppCarnet/forms.py
+++ ProyectoCarnet/AppCarnet/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 
 
@@ -19,8 +20,49 @@ class cargarPersona(forms.ModelForm):
         model = Usuario
         fields = ('first_name', 'last_name', 'dni', 'nacimiento')
     
+class CustomUserCreationForm(forms.Form):
+    username = forms.CharField(label='Ingrese un nombre de usuario', min_length=4, max_length=150)
+    email = forms.EmailField(label='Ingrese un email')
+    password1 = forms.CharField(label='Ingrese una contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirme la contraseña', widget=forms.PasswordInput)
 
-class CreateUserForm(forms.ModelForm):
+    def clean_username(self):
+        username = self.cleaned_data['username'].lower()
+        r = Usuario.objects.filter(username=username)
+        if r.count():
+            raise  ValidationError("Ese nombre de usuario ya existe.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        r = Usuario.objects.filter(email=email)
+        if r.count():
+            raise  ValidationError("Ese email ya esta registrado.")
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Contraseñas no coinciden")
+
+        return password2
+
+    def save(self, commit=True):
+        user = Usuario.objects.create_user(
+            self.cleaned_data['username'],
+            self.cleaned_data['email'],
+            self.cleaned_data['password1']
+        )
+        return user
+
+
+
+
+
+
+"""class CreateUserForm(forms.ModelForm):
     email = forms.EmailField(required=True, label='Email',error_messages={'exists': 'This already exists!'})
     password1 = forms.CharField(max_length=30)
     password2 = forms.CharField(max_length=30)
@@ -39,4 +81,4 @@ class CreateUserForm(forms.ModelForm):
     def clean_email(self):
         if User.objects.filter(email=self.cleaned_data['email']).exists():
             raise forms.ValidationError(self.fields['email'].error_messages['exists'])
-        return self.cleaned_data['email']
+        return self.cleaned_data['email']"""
