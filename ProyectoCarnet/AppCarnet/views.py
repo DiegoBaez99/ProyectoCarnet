e1e04ad40640ec8f93a8b7c3d3cfdf578996ae9e
Gabriel Andino <gabriel_andino@outlook.com>
2020-09-15 20:15:10 -0300

    Agrego el login view

--- ProyectoCarnet/AppCarnet/views.py
+++ ProyectoCarnet/AppCarnet/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate
 from django.http import HttpResponse, Http404
 from .forms import forms, CustomUserCreationForm, cargarDireccion, cargarPersona
 from .models import Direcciones
@@ -21,22 +21,30 @@ def signup(request):
 
     return render(request, 'signup.html', {'form': f})
 
-def loginPage(request):
-    if(request.user.is_authenticated):
+def login(request):
+    #Si el usuario ya esta logueado que lo mande a la pag principal
+    if request.user.is_authenticated:
         return redirect('index')
-    else:
-        if(request.method == 'POST'):
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(request, username=username, password=password) #name q esta en el html
-            if(user is not None):
-                login(request, user)     
-                return redirect('index')
-            else:
-                messages.info(request, "Nombre de usuario o contraseña incorrecto.")
 
-    context = {}
-    return render(request, 'login.html', context)
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            # correct username and password login the user
+            auth.login(request, user)
+            return redirect('index')
+
+        else:
+            messages.error(request, 'Error nombre de usuario o contraseña incorrecto.')
+
+    return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return render(request,'logout.html')
 
 
 def persona(request):
