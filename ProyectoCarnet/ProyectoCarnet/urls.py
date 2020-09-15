ab79fdffaae55c95da0f24f4b36c0f3823860439
Gabriel Andino <gabriel_andino@outlook.com>
2020-09-15 20:16:21 -0300

    Agrego las urls y toco algo del settings XD

--- ProyectoCarnet/ProyectoCarnet/urls.py
+++ ProyectoCarnet/ProyectoCarnet/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from AppCarnet import views
 
+
 urlpatterns = [
+    path('login/',views.login,  name='login'),
+    path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
-    path('login/', views.loginPage, name='login'),
-    path('logout/', views.logoutUser, name='logout'),
     path('datos', views.persona, name='datos'),
     path('direccion/', views.direccion, name='direccion'),
     path('admin/', admin.site.urls),
