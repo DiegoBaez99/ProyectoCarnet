ce025d207f3c1d718b27137b957e77c1ce29b348
benitez96 <danii-benitez@hotmail.com>
2020-09-16 19:54:20 -0300

    agrego funcionalidades a Panel Administracion

--- ProyectoCarnet/user/admin.py
+++ ProyectoCarnet/user/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from user.models import Usuario
 
-admin.site.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('dni', 'first_name', 'last_name','validado' ,'grupo_s', 'nacimiento',)
+    list_filter = ('validado', 'grupo_s',)
+    search_fields = ('dni', 'first_name', 'last_name',)
+    
+
+
+admin.site.register(Usuario, UsuarioAdmin)
