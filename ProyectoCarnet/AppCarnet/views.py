4e1fbbe53e42a7963f3c8ad2027bd6c474777df3
Lucas Carabajal <lucascar007@gmail.com>
2020-09-21 20:37:13 -0300

    Carga de imagen

--- ProyectoCarnet/AppCarnet/views.py
+++ ProyectoCarnet/AppCarnet/views.py
@@ -60,18 +60,7 @@ class CrearCarnet(CreateView):
     form_class = CarnetForm
     template_name = 'carnet.html'
     success_url = reverse_lazy('home')
-
-    def upload_image_view(request):
-        if request.method == 'GET':
-            form = CarnetForm(request.GET, request.FILES)
-            if form.is_valid():
-                form.save()
-                message = "Image uploaded succesfully!"
-        else:
-            form = CarnetForm()
-
-        return render('carnet.html', locals())
-
+  
 class CrearCedula(CreateView):
     model = Cedula
     form_class = CedulaForm
--- ProyectoCarnet/ProyectoCarnet/settings.py
+++ ProyectoCarnet/ProyectoCarnet/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
